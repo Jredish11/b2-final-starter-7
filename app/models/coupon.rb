6213233ebb314b6345db9ca71a3a8c09a8f3f719
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-06-11 08:42:11 -0600

    add presence and uniqueness for validations

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -3,9 +3,9 @@ class Coupon < ApplicationRecord
   has_many :invoices
 
   validates :coupon_name, presence: true
-  validates :coupon_code, presence: true
-  validates :discount_amount, numericality: { greater_than: 0 }
-  validates :discount_type, inclusion: { in: ['percent_off', 'dollar_off'] }
+  validates :coupon_code, presence: true, uniqueness: true
+  validates :discount_amount, presence: true, numericality: { greater_than: 0 }
+  validates :discount_type, presence: true, inclusion: { in: ['percent_off', 'dollar_off'] }
 
   enum status: [:deactivated, :activated]
   enum discount_type: [:percent_off, :dollar_off]

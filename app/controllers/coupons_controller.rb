370b46a38b25be008663ce1303612f56729510da
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-06-10 22:05:52 -0600

    feat: fix new and create actions to help with render failure

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,15 +1,32 @@
 class CouponsController < ApplicationController
   def index
-    @coupons = Coupon.all
     @merchant = Merchant.find(params[:merchant_id])
+    @coupons = @merchant.coupons
   end
 
   def show
-    # @merchant = Merchant.find(params[:merchant_id])
-    # @coupon = Coupon.find(params[:id])
-    # if @coupon.nil?
-    #   flash[:error] = "Coupon not found"
-    #   redirect_to merchant_coupons_path(@merchant)
-    # end
+ 
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+    
+  end
+
+  def create
+    merchant = Merchant.find(params[:merchant_id])
+    coupon = merchant.coupons.new(coupon_params)
+    if coupon.save
+      redirect_to merchant_coupons_path(merchant)
+    else
+      redirect_to new_merchant_coupon_path(merchant)
+      flash[:alert] = "Form filled incorrectly. Please check your inputs."
+    end
+  end
+end
+
+private
+
+def coupon_params
+  params.permit(:coupon_name, :coupon_code, :discount_amount, :discount_type, :status)
 end

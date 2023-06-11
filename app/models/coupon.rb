19a4a877047e85f4b58d7d517058fe6917202db6
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-06-10 22:06:07 -0600

    add validations for new coupon form

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -2,6 +2,12 @@ class Coupon < ApplicationRecord
   belongs_to :merchant
   has_many :invoices
 
+  validates :coupon_name, presence: true
+  validates :coupon_code, presence: true
+  validates :discount_amount, numericality: { greater_than: 0 }
+  validates :discount_type, inclusion: { in: ['percent_off', 'dollar_off'] }
+
   enum status: [:deactivated, :activated]
   enum discount_type: [:percent_off, :dollar_off]
+
 end

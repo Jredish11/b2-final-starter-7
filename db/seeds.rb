c2a41251847c212f9cdbf85752086d89deef8dba
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-06-14 08:09:41 -0600

    try again

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,8 @@
 # Transaction.destroy_all
 # Item.destroy_all
 # Invoice.destroy_all
-# Coupon.destroy_all
-Customer.destroy_all
+Coupon.destroy_all
+# Customer.destroy_all
 # Merchant.destroy_all
 Rake::Task["csv_load:all"].invoke
 @merchant1 = Merchant.create!(name: "Hair Care")
